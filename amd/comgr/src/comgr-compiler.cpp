GZGavinZhao/llvm-project

[Comgr] Extend ISA compatibility for CCOB
Signed-off-by: Gavin Zhao <git@gzgz.dev>

amd/comgr/src/comgr-compiler.cpp
@@ -39,6 +39,7 @@
 #include "comgr-compiler.h"
 #include "comgr-device-libs.h"
 #include "comgr-env.h"
+#include "comgr-metadata.h"
 #include "lld/Common/CommonLinkerContext.h"
 #include "lld/Common/Driver.h"
 #include "clang/Basic/Version.h"
@@ -1298,9 +1299,63 @@ amd_comgr_status_t AMDGPUCompiler::unbundle() {
     size_t index = output_prefix.find_last_of(".");
     output_prefix = output_prefix.substr(0, index);
 
+    std::set<std::string> co_entry_ids;
+    if (auto Error = OffloadBundler::GetBundleIDsInFile(input_file_path, BundlerConfig, co_entry_ids)) {
+      if (env::shouldEmitVerboseLogs()) {
+        LogS << "Failed to get bundle IDs in "
+             << input_file_path
+             << ", not trying to coerce compatible archs\n";
+        LogS.flush();
+      }
+    }
+    std::set<std::pair<std::string, std::string>> entry_ids_with_idents;
+    for (const auto& co_entry_id : co_entry_ids) {
+      std::string target_ident;
+      size_t pos = std::string::npos;
+      for (const auto& prefix : {"hipv4-", "hip-"}) {
+        if ((pos = co_entry_id.find(prefix)) == 0) {
+          target_ident = co_entry_id.substr(std::char_traits<char>::length(prefix));
+          break;
+        }
+      }
+
+      if (target_ident.empty()) {
+        continue;
+      }
+
+      entry_ids_with_idents.emplace(co_entry_id, target_ident);
+    }
+
     // Bundler target and output names
     for (auto entry: ActionInfo->BundleEntryIDs) {
-      BundlerConfig.TargetNames.push_back(entry);
+      std::string compat_entry = entry;
+
+      std::string entry_ident;
+      size_t pos = std::string::npos;
+      for (const auto& prefix : {"hipv4-", "hip-"}) {
+        if ((pos = entry.find(prefix)) == 0) {
+          entry_ident = entry.substr(std::char_traits<char>::length(prefix));
+          break;
+        }
+      }
+
+      if (!entry_ident.empty()) {
+        std::string best_entry;
+        for (const auto& [co_entry, co_ident] : entry_ids_with_idents) {
+          if (entry_ident == co_ident) {
+            best_entry = co_entry;
+            break;
+          } else if (metadata::isCompatibleIsaName(entry_ident, co_ident)) {
+            best_entry = co_entry;
+          }
+        }
+
+        if (!best_entry.empty()) {
+          compat_entry = best_entry;
+        }
+      }
+
+      BundlerConfig.TargetNames.push_back(compat_entry);
 
       // Add an output file for each target
       std::string output_file_name = output_prefix + '-' + entry + "." +

GZGavinZhao/rocm-llvm-project

amd/comgr/src/comgr-compiler.cpp
@@ -20,6 +20,7 @@
 #include "comgr-env.h"
 #include "comgr-spirv-command.h"
 #include "comgr-unbundle-command.h"
+#include "comgr-metadata.h"
 #include "lld/Common/CommonLinkerContext.h"
 #include "lld/Common/Driver.h"
 #include "clang/CodeGen/CodeGenAction.h"
@@ -1378,17 +1379,72 @@ amd_comgr_status_t AMDGPUCompiler::unbundle() {
            << "   Unbundled Files Extension: ." << FileExtension << "\n";
     }
 
+    std::set<std::string> CoEntryIds;
+    if (auto Error = OffloadBundler::GetBundleIDsInFile(InputFilePath, BundlerConfig, CoEntryIds)) {
+      if (env::shouldEmitVerboseLogs()) {
+        LogS << "Failed to get bundle IDs in "
+             << InputFilePath
+             << ", not trying to coerce compatible archs\n";
+        LogS.flush();
+      }
+    }
+    std::set<std::pair<std::string, std::string>> entry_ids_with_idents;
+    for (const auto& CoEntryId : CoEntryIds) {
+      std::string TargetIdent;
+      size_t Pos = std::string::npos;
+      for (const auto& Prefix : {"hipv4-", "hip-"}) {
+        if ((Pos = CoEntryId.find(Prefix)) == 0) {
+          TargetIdent = CoEntryId.substr(std::char_traits<char>::length(Prefix));
+          break;
+        }
+      }
+
+      if (TargetIdent.empty()) {
+        continue;
+      }
+
+      entry_ids_with_idents.emplace(CoEntryId, TargetIdent);
+    }
+
+    // Bundler target and output names
     for (StringRef Entry : ActionInfo->BundleEntryIDs) {
+      std::string CompatEntry = Entry.str();
+
+      std::string EntryIdent;
+      size_t Pos = std::string::npos;
+      for (const auto& Prefix : {"hipv4-", "hip-"}) {
+        if ((Pos = Entry.find(Prefix)) == 0) {
+          EntryIdent = Entry.substr(std::char_traits<char>::length(Prefix));
+          break;
+        }
+      }
+
+      if (!EntryIdent.empty()) {
+        std::string BestEntry;
+        for (const auto& [CoEntry, CoIdent] : entry_ids_with_idents) {
+          if (EntryIdent == CoIdent) {
+            BestEntry = CoEntry;
+            break;
+          } else if (metadata::isCompatibleIsaName(EntryIdent, CoIdent)) {
+            BestEntry = CoEntry;
+          }
+        }
+
+        if (!BestEntry.empty()) {
+          CompatEntry = BestEntry;
+        }
+      }
+
       // Add an output file for each target
       SmallString<128> OutputFilePath = OutputDir;
       sys::path::append(OutputFilePath,
-                        OutputPrefix + "-" + Entry + "." + FileExtension);
+                        OutputPrefix + "-" + CompatEntry + "." + FileExtension);
 
-      BundlerConfig.TargetNames.emplace_back(Entry);
+      BundlerConfig.TargetNames.emplace_back(CompatEntry);
       BundlerConfig.OutputFileNames.emplace_back(OutputFilePath);
 
       if (env::shouldEmitVerboseLogs()) {
-        LogS << "\tBundle Entry ID: " << Entry << "\n"
+        LogS << "\tBundle (Compat)Entry ID: " << CompatEntry << "\n"
              << "\tOutput Filename: " << OutputFilePath << "\n";
         LogS.flush();
       }